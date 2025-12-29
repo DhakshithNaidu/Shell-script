48b38ba180da354ca93093a2f235cd36d6b91a24
devops-76s <kasi.bommu@gmail.com>
2025-12-29 07:59:04 +0530

    Exceuting 5th Script

---
+++ 05-variable-read.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter your username ::"
+
+read -s USERNAME # the value entred above will be automatically attached to USERNAME Variable
+
+echo "Please enter your password ::"
+
+echo "Username is : $USERNAME , Password is : $PASSWORD " # i am printing just for validation you should not print username and password in the script
