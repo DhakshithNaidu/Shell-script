bc048eaaa82c58237e04f6fa49604edbe49c0d36
devops-76s <kasi.bommu@gmail.com>
2025-12-30 06:16:32 +0530

    Excuting 7th script

---
+++ 07-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FRUITS=("Apple" "Banana" "Orange")
+echo "First Value : ${FRUITS[0]}"
+echo "Second Value : ${FRUITS[1]}"
+echo "Third Value : ${FRUITS[2]}"
+echo "All Fruits : ${FRUITS[@]}"
