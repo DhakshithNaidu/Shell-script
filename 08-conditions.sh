1437d2f456f1daf262c4a099befb4e8780b42ea5
devops-76s <kasi.bommu@gmail.com>
2025-12-31 06:20:26 +0530

    conition

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBER=$1
-if[ $NUMBER -gt 100 ]
-then
+if [ $NUMBER -gt 100 ]
+ then
     echo "give number $NUMBER is greater then 100"
-else
+ else
     echo "give number $NUMBER is not greater then 100"
 fi        
